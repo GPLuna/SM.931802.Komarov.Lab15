7a0bbf98bfd95803388887624559b73f4d7c1327
GPLuna <GPLuna@mail.ru>
2021-06-17 21:29:53 +0700

    Добавьте файлы проекта.

---
+++ Lab15/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿
+namespace Lab15
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.N = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Sunny = new System.Windows.Forms.TextBox();
+            this.Cloudy = new System.Windows.Forms.TextBox();
+            this.Rain = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.N)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(64, 160);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "N";
+            // 
+            // N
+            // 
+            this.N.Location = new System.Drawing.Point(98, 158);
+            this.N.Name = "N";
+            this.N.Size = new System.Drawing.Size(59, 23);
+            this.N.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.button1.Location = new System.Drawing.Point(265, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 100);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Sunny";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(30, 78);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Cloudy";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(42, 117);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 17);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Rain";
+            // 
+            // Sunny
+            // 
+            this.Sunny.Location = new System.Drawing.Point(98, 37);
+            this.Sunny.Name = "Sunny";
+            this.Sunny.Size = new System.Drawing.Size(100, 23);
+            this.Sunny.TabIndex = 8;
+            // 
+            // Cloudy
+            // 
+            this.Cloudy.Location = new System.Drawing.Point(98, 75);
+            this.Cloudy.Name = "Cloudy";
+            this.Cloudy.Size = new System.Drawing.Size(100, 23);
+            this.Cloudy.TabIndex = 9;
+            // 
+            // Rain
+            // 
+            this.Rain.Location = new System.Drawing.Point(98, 114);
+            this.Rain.Name = "Rain";
+            this.Rain.Size = new System.Drawing.Size(100, 23);
+            this.Rain.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(436, 313);
+            this.Controls.Add(this.Rain);
+            this.Controls.Add(this.Cloudy);
+            this.Controls.Add(this.Sunny);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.N);
+            this.Controls.Add(this.label2);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.N)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown N;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox Sunny;
+        private System.Windows.Forms.TextBox Cloudy;
+        private System.Windows.Forms.TextBox Rain;
+    }
+}
+
---
+++ Lab15/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO;
+
+namespace Lab15
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public string Forecast;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Imitation model = new Imitation((int)N.Value);
+            model.GetValue();
+            model.GetFreq();
+            Sunny.Text = Convert.ToString(Math.Round(model.freq[0], 3));
+            Rain.Text = Convert.ToString(Math.Round(model.freq[1], 3));
+            Cloudy.Text = Convert.ToString(Math.Round(model.freq[2], 3));
+            String filename = "Forecast.txt";
+            System.Diagnostics.Process.Start(filename);
+            
+        }
+        class Imitation
+        {
+            int N;
+            int i;
+            double Time = 0;
+            double t1;
+            double t0 = 0;
+            int day = 1;
+            int month = 06;
+            int hour = 0;
+            int minute;
+            int c = 0;
+            int c1 = 0;
+            public string[] Weather = new string[] { "ясно", "облачно", "дождь" };
+            public double[] freq = new double[3] { 0, 0, 0 };
+            double[,] Matrix = new double[3, 3] { { -0.4, 0.3, 0.1 }, { 0.4, -0.8, 0.4 }, { 0.1, 0.4, -0.5 } };
+            double[] P = new double[] { 0.0, 0.0, 0.0 };
+            Random rnd = new Random();
+            public Imitation(int n)
+            {
+                N = n;
+            }
+            public void GetValue()
+            {
+                int q = 0;
+                Time = 0;
+                string writePath = "Forecast.txt";
+                try
+                {
+                    using (StreamWriter output = new StreamWriter(writePath, false, System.Text.Encoding.Default))
+                        while (q < N)
+                   {
+
+                        double a = rnd.NextDouble();
+                            t1 = Math.Log(a) / Matrix[i, i];
+                            int i_new = -1;
+                            do
+                            {
+                                i_new++;
+                                if (i_new != i)
+                                {
+                                    a -= -Matrix[i, i_new] / Matrix[i, i];
+                                }
+                            } while (a > 0);
+                            P[i] += Math.Abs(t0 - t1);
+                            Time += t0;
+                        output.WriteLine(GetTime(Time, i));
+                        t0 = t1;
+                            i = i_new;
+                            q++;
+                        }
+                }
+                catch (Exception e)
+                {
+                }
+            }
+
+            public string GetTime(double t, int i)
+            {
+                string data = "";
+                t -= 30 * c1;
+                if (Math.Truncate(t) != day)
+                {
+                    t -= Math.Truncate(t);
+                    if (day > 30)
+                    {
+                        month++;
+                        day -= 30;
+                        c1++;
+                        if (month == 13)
+                        {
+                            month = 1;
+                        }
+                    }
+                    
+                    data += day + "." + month + "   ";
+                    day++;
+                }
+                else
+                {
+                    if (c == 0)
+                    {
+                        data += "1.6    ";
+                    }
+                    else
+                    {
+                        data += "       ";
+                    }
+                    t -= Math.Truncate(t);
+                }
+                hour = (int)Math.Truncate(24 * t);
+                data += hour + ":";
+                minute = (int)Math.Truncate(60 * (24 * t - Math.Truncate(24 * t)));
+                data += minute + "  ";
+                data += Weather[i];
+                c++;
+                return data;
+            }
+            public void GetFreq()
+            {
+                double longTime = 0;
+                for (int j = 0; j < P.Length; j++)
+                {
+                    longTime += P[j];
+                }
+
+                for (int j = 0; j < freq.Length; j++)
+                {
+                    freq[j] = P[j] / longTime;
+                }
+            }
+            
+        }
+    }
+}
